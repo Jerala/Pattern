0fc79c534501c1416701e4c1e961950378305aad
jerala <jerala@mail.ru>
2020-09-15 19:14:07 +0300

    init

--- src/study/mirea/Main.java
+++ src/study/mirea/Main.java
@@ -7,33 +7,34 @@ import java.util.Scanner;
 
 public class Main {
 
-//    private List<String> stringList1 = new ArrayList<>(Arrays.asList("SomeString", "Patterns", "123"));
+    //    private List<String> stringList1 = new ArrayList<>(Arrays.asList("SomeString", "Patterns", "123"));
     private static List<String> stringList2 = new ArrayList<>(Arrays.asList("SecondArray", "StringToo", "", "Zero"));
 
     public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    StringSorter stringSorter = null;
-	    boolean isCorrectNumberEntered = false;
+        StringSorter stringSorter = getSorter();
+        List<String> result = stringSorter.sort(stringList2);
+        System.out.println(result.toString());
+    }
+
+    private static StringSorter getSorter() {
+        Scanner sc = new Scanner(System.in);
+        boolean isCorrectNumberEntered = false;
         int number;
         while (!isCorrectNumberEntered) {
             try {
                 System.out.println("Enter a number: ");
                 number = Integer.parseInt(sc.nextLine());
-                stringSorter = getSorter(number);
                 isCorrectNumberEntered = true;
+                if (number % 2 == 0) {
+                    return new StringByASCIISorter();
+                }
+                return new StringByLengthSorter();
             } catch (Exception e) {
                 System.out.println("Exception: " + e.getLocalizedMessage());
+                continue;
             }
         }
-        List<String> result = stringSorter.sort(stringList2);
-        System.out.println(result.toString());
-    }
-
-    private static StringSorter getSorter(int number) {
-        if (number % 2 == 0) {
-            return new StringByASCIISorter();
-        }
-        return new StringByLengthSorter();
+        return null;
     }
 
 }
